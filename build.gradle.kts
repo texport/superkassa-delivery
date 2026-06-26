46acf12436c923bbf4d5fa673463157fc363f0a2
Sergey Ivanov <ivanov.sergey.ekb@gmail.com>
2026-06-26 19:21:34 +0700

    Configure Maven Central publishing with Sonatype NMCP portal integration and GPG signing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,78 @@
 plugins {
     alias(libs.plugins.detekt)
     alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.nmcp)
     `maven-publish`
+    signing
     jacoco
 }
 
 group = "io.github.texport"
 version = libs.versions.superkassaDelivery.get()
 
+val sourcesJar = tasks.register<Jar>("sourcesJar") {
+    description = "Generates the sources JAR artifact"
+    archiveClassifier.set("sources")
+    from(sourceSets.main.get().allSource)
+}
+
+val javadocJar = tasks.register<Jar>("javadocJar") {
+    description = "Generates the javadoc JAR artifact"
+    archiveClassifier.set("javadoc")
+    from(tasks.javadoc)
+}
+
 publishing {
     publications {
         create<MavenPublication>("mavenJava") {
             from(components["java"])
+            artifact(sourcesJar)
+            artifact(javadocJar)
+
+            pom {
+                name.set("superkassa-delivery")
+                description.set("Unified delivery dispatcher abstraction for Superkassa fiscal receipts")
+                url.set("https://github.com/texport/superkassa-delivery")
+
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("sergeyivanov")
+                        name.set("Sergey Ivanov")
+                        email.set("ivanov.sergey.ekb@gmail.com")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git://github.com/texport/superkassa-delivery.git")
+                    developerConnection.set("scm:git:ssh://github.com/texport/superkassa-delivery.git")
+                    url.set("https://github.com/texport/superkassa-delivery")
+                }
+            }
         }
+    }
+}
+
+signing {
+    val signingKey = System.getenv("SIGNING_KEY")
+    val signingPassword = System.getenv("SIGNING_PASSWORD")
+    if (!signingKey.isNullOrEmpty() && !signingPassword.isNullOrEmpty()) {
+        useInMemoryPgpKeys(signingKey, signingPassword)
+    }
+    sign(publishing.publications["mavenJava"])
+}
+
+nmcp {
+    publishAllPublicationsToCentralPortal {
+        username.set(project.findProperty("ossrhUsername")?.toString() ?: System.getenv("OSSRH_USERNAME"))
+        password.set(project.findProperty("ossrhPassword")?.toString() ?: System.getenv("OSSRH_PASSWORD"))
+        publishingType.set("AUTOMATIC")
     }
 }
 
